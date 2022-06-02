ec4d99d8d2cc8c38eaccac4804f39c1ffb342068
tasca-a <a.tasca@campus.unimib.it>
2022-06-02 10:26:30 +0200

    Added ex. 4-10.

---
+++ Chapter 4/4-10.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h> /* for atof() */
+#include <ctype.h>
+#include <math.h>
+
+#define MAXOP   100 /* max size of operand or operator */
+#define NUMBER  '0' /* signal that a number was found */
+#define MAXVAL 100  /* maximum depth of val stack */
+#define BUFSIZE 100
+#define MAXVAR 26   /* maximum variables number */
+
+void push(double);
+double pop(void);
+void printtop(void);            
+void duplicatetop(void);        
+void swaptopelements(void);     
+void clearstack(void);          
+void printallstack(void);
+
+int getop(char []);
+
+void mgetline(char []);
+
+int sp = 0;         /* next free stack position */
+double val[MAXVAL]; /* value stack */
+
+double var[MAXVAR]; /* array where variables are stored */
+double recent = 0;      /* most recent printed value */
+char recentvar = 'A';   /* most recent variable used */
+
+/* reverse Polish calculator */
+int main(){
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    for (int i = 0; i < MAXVAR; i++)    // clear the var array
+        var[i] = 0;
+
+    while ((type = getop(s)) != EOF){
+        switch (type){
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '%':
+                op2 = pop();
+                push((int) pop() % (int) op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero division\n");
+                break;
+            case 's':
+                push(sin(pop()));
+                break;
+            case 'e':
+                push(exp(pop()));
+                break;
+            case 'p':
+                op2 = pop();
+                push(pow(pop(), op2));
+                break;
+            case 'r':
+                printf("\t%.8g\n", recent);
+                break;
+            case '=':
+                pop(); 
+                var[recentvar-'A'] = pop();
+                break;
+            case '\n':
+                recent = pop();
+                printf("\t%.8g\n", recent);
+                break;
+            default:
+                if (s[0] >= 'A' && s[0] <= 'Z'){ // it's a variable!
+                    push(var[s[0]-'A']);
+                    recentvar = s[0];
+                }
+                else
+                    printf("error: unknowkn command: \"%s\"\n", s);
+                break;
+        }
+    }
+
+    return 0;
+}
+
+/* push: push f onto value stack */
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else{
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+/* printtop: print the top element of the stack without popping */
+void printtop(void)
+{
+    int tmp;
+    if (sp > 0){
+        tmp = sp - 1;
+        printf("top element of the stack: %f\n", val[tmp]);
+    } else
+        printf("error: stack empty\n");
+}
+
+/* duplicatetop: duplicate the top element of the stack */
+void duplicatetop(void)
+{
+    double tmp;
+    if (sp > 0){
+        tmp = val[sp-1];
+        val[sp++] = tmp;
+    } else
+        printf("error: stack empty\n");
+}
+
+/* swaptopelements: swap the two top elements of the stack */
+void swaptopelements(void)
+{
+    double top, bottom;
+    double tmp;
+
+    if (sp > 1)
+        tmp = val[sp-2], val[sp-2] = val[sp-1], val[sp-1] = tmp;
+    else
+        printf("error: too few elements on the stack\n");
+}
+
+/* clearstack: clear the stack */
+void clearstack(void)
+{
+    sp = 0;
+}
+
+/* printallstack: print all the stack */
+void printallstack(void)
+{
+    for (int i = 0; i < sp; i++)
+        printf("%f ", val[i]);
+    putchar('\n');
+}
+
+int lastopindex = 0;
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+    char input[BUFSIZE];
+    int i, c, n;
+
+    if (lastopindex > 0 && input[lastopindex-1] == '\0')
+        return EOF;
+
+    mgetline(input);
+
+    s[0] = '+';
+
+    while ((s[1] = c = input[lastopindex++]) == ' ' || c == '\t')    // remove trailing blanks and tabs
+        ;
+
+    if (c == '-'){
+        if (isdigit(n = input[lastopindex++]) || n == '.'){
+            s[0] = c;
+            c = n;
+        }
+
+        if (n == '\n')
+            lastopindex--;
+    }
+
+    lastopindex--;
+
+    for (i = 1; isdigit(s[i] = c = input[lastopindex++]) || s[i] == '.'; i++)
+        ;
+
+    s[i+1] = '\0';
+
+    if (i == 1){
+        s[0] = c, s[1] = '\0';
+        return c;
+    }
+    else
+        return NUMBER;
+}
+
+/* MOCK mgetline: get an entire line from input */
+void mgetline(char s[])
+{
+    char in[] = "45 45 +\n";
+
+    for (int i = 0; in[i] != '\0'; i++)
+        s[i] = in[i];
+}
