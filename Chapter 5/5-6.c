75e2a10c7dd40999e2c058704f4892a1a2eba994
tasca-a <a.tasca@campus.unimib.it>
2022-06-10 18:02:51 +0200

    Added itoa and reverse functions to ex. 5-6.

--- Chapter 5/5-6.c
+++ Chapter 5/5-6.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #define SIZE 100
 
 int mgetline(char *, int);
 int atoi(char *);
+char *itoa(int, char *);
+void reverse(char *s);
 
 int main()
 {
@@ -17,6 +20,9 @@ int main()
 
     // atoi test
     printf("String: %s\tNumber: %d\n", "-4586", atoi("-4586"));
+
+    // itoa test
+    printf("Number: %d\tString: %s\n", -234, itoa(-234, str1));
 
     return 0;
 }
@@ -50,13 +56,43 @@ int atoi(char *s)
     return sign * out;
 }
 
+/* itoa:    convert n to characters in s */
+char *itoa(int n, char *s)
+{
+    int sign;
+    char *start = s;
 
+    if ((sign = n) < 0)
+        n = -n;
 
+    do {
+        *s++ = n % 10 + '0';
+    } while ((n /= 10) > 0);
 
+    if (sign < 0)
+        *s++ = '-';
+    
+    *s = '\0';
+    
+    reverse(start);
+
+    return start;
+}
+
+/* reverse: reverse the string passed in input */
+void reverse(char *s)
+{
+    char *start, *end;
+    char tmp;
+
+    for (end = start = s; *end != '\0'; end++)
+        ;
+    
+    for (end -= 1; start < end; start++, end--)
+        tmp = *start, *start = *end, *end = tmp;
+}
 
 /*
-    itoa
-    reverse
     strindex
     getop
 */
