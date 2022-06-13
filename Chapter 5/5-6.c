fd1b5b22c17dbd41b34ce882087a8f062de4a6cc
Alessandro Tasca <a.tasca@campus.unimib.it>
2022-06-13 14:04:41 +0200

    Added strindex() to 5-6. NOT WORKING.

--- Chapter 5/5-6.c
+++ Chapter 5/5-6.c
@@ -8,6 +8,7 @@ int mgetline(char *, int);
 int atoi(char *);
 char *itoa(int, char *);
 void reverse(char *s);
+int strindex(char *s, char *t);
 
 int main()
 {
@@ -23,6 +24,9 @@ int main()
 
     // itoa test
     printf("Number: %d\tString: %s\n", -234, itoa(-234, str1));
+
+    // strindex test
+    printf("String: %s\tOther: %s\tStrindex: %d\n", "quaqquew", "uew", strindex("quaqquew", "uew"));
 
     return 0;
 }
@@ -90,6 +94,39 @@ void reverse(char *s)
     
     for (end -= 1; start < end; start++, end--)
         tmp = *start, *start = *end, *end = tmp;
+}
+
+/* strindex:    return index of t in s, -1 if none */
+/* NOT WORKING YET */
+int strindex(char *s, char *t)
+{
+    int ls, lt;
+    int i, l;
+
+    // get lengths
+    for (ls = 0; *s++ != '\0'; ls++)
+        ;
+    for (lt = 0; *t++ != '\0'; lt++)
+        ;
+
+    printf("%d\t%d\n", ls, lt);
+
+    // restore pointers
+    s -= ls + 1;
+    t -= lt + 1;
+
+    for (i = 0; i < (ls - lt); i++){
+        for (l = 0; *s == *t; l++){
+            t++;
+        }
+
+        if (*t == '\0')
+            return i;
+        else
+            t -= l;
+    }
+
+    return -1;
 }
 
 /*
